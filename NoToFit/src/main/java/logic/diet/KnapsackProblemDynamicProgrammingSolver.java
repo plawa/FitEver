881d44691e995036083fd4f7845c82d6d7ab15fb
Piotr Ławniczak <thestuntman@outlook.com>
2016-10-21 23:56:00 +0200

    Knapsack problem ignited

--- NoToFit/src/main/java/logic/diet/KnapsackProblemDynamicProgrammingSolver.java
+++ NoToFit/src/main/java/logic/diet/KnapsackProblemDynamicProgrammingSolver.java
@@ -4,7 +4,7 @@ public class KnapsackProblemDynamicProgrammingSolver {
 
 	static int max(int a, int b) {
 		return (a > b) ? a : b;
-	}
+	}/*
 
 	// Returns the maximum value that can be put in a knapsack of capacity W
 	static int knapSack(int capacity, int wt[], int val[], int n) {
@@ -23,7 +23,7 @@ public class KnapsackProblemDynamicProgrammingSolver {
 		else
 			return max(val[n - 1] + knapSack(capacity - wt[n - 1], wt, val, n - 1), knapSack(capacity, wt, val, n - 1));
 	}
-
+`*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- NoToFit/src/main/java/logic/diet/MealChooser.java
+++ NoToFit/src/main/java/logic/diet/MealChooser.java
@@ -3,6 +3,7 @@ package logic.diet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 import database.controller.DatabaseController;
 import database.entities.Meal;
@@ -27,7 +28,7 @@ public class MealChooser {
 	public static Set<Meal> chooseDayMealSet(int caloriesPerDay, int mealsPerDay) {
 		Set<Meal> dayMealSet = new HashSet<Meal>();
 		int dietCalories = 0;
-		
+
 		for (Meal meal : mainMealsLibrary) {
 			int mealCalories = MealTools.countMealCalories(meal);
 			dietCalories += mealCalories;
@@ -39,28 +40,66 @@ public class MealChooser {
 		}
 		return dayMealSet;
 	}
-	
-	private static Set<Meal> chooseBreakfastMealSet(int breakfastCalories, int breakfastMealCount){
+
+	private static Set<Meal> chooseBreakfastMealSet(int breakfastCalories, int breakfastMealCount) {
 
 		Set<Meal> breakfastsChosen = new HashSet<Meal>();
-		
+
 		return breakfastsChosen;
 	}
-	
-	private static Set<Meal> chooseOptimalSubset(int itemsCount, int value){
-		
-		
-		
-		
+
+	private static Set<Meal> chooseOptimalSubset(int itemsCount, int value) {
+
 		return null;
+
+	}
+
+	static Tuple max(Tuple cas1, Tuple cas2) {
+		return (cas1.kCalsAvailable < cas2.kCalsAvailable) ? cas1 : cas2;
+	}
+
+	static int knapSack(final int kCalsInitiallyAvailable, List<Meal> mealsLib, int n) {
 		
+		Stack<Tuple> st = new Stack<>();
+		st.push(new Tuple(kCalsInitiallyAvailable, n));	
+		
+		while (!st.empty()) {
+			Tuple cas = st.pop();
+			if (cas.n == 0 || cas.kCalsAvailable == 0){
+				return kCalsInitiallyAvailable - cas.kCalsAvailable;
+			}
+
+			int kCal = MealTools.countMealCalories(mealsLib.get(cas.n - 1));
+			if (kCal > cas.kCalsAvailable) {
+				Tuple newCas = new Tuple(cas.kCalsAvailable, cas.n - 1);
+				st.push(newCas);
+			} else {
+				Tuple cas1 = new Tuple(cas.kCalsAvailable - kCal, cas.n - 1);
+				Tuple cas2 = new Tuple(cas.kCalsAvailable, cas.n - 1);
+				
+				st.push(max(cas1, cas2));
+				//iter.add(max(cas1, cas2));
+			}
+		}
+		return kCalsInitiallyAvailable;
 	}
 
 	public static void main(String[] args) {
-		updateLibraries();
-		Set<Meal> testMealList = chooseDayMealSet(3000, 5);
-		for (Meal mealSelected : testMealList) {
-			System.out.println(mealSelected.getName());
+		System.out.println(knapSack(4541, mainMealsLibrary, mainMealsLibrary.size()));
+		 //Set<Meal> testMealList = chooseDayMealSet(3000, 5);
+		// for (Meal mealSelected : testMealList) {
+		// System.out.println(mealSelected.getName());	
+		//}
+	}
+
+	public static class Tuple {
+
+		public Integer kCalsAvailable;
+		public Integer n;
+
+		public Tuple(Integer kCalsAvailable, Integer n) {
+			this.kCalsAvailable = kCalsAvailable;
+			this.n = n;
 		}
 	}
 
